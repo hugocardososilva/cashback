f71b94b71d0e9b17c128d5136c6eb2ad9cf07e2f
Hugo Silva <hugocardososilva@gmail.com>
2020-09-29 01:55:14 -0300

    feat(offer): models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,31 +1,26 @@
 # frozen_string_literal: true
 
 class Offer < ApplicationRecord
+  include OfferAdmin
   validates :advertiser_name, :url, :description, :starts_at, presence: true
   validates :url, url: true
   validates :description, length: { maximum: 500 }
   validates :advertiser_name, uniqueness: true
   validate :starts_at_before_ends_at
 
-  scope :enabled, lambda {
-    select { |resource| resource.status == 'enabled' }
-  }
-  scope :disabled, lambda {
-    select { |resource| resource.status == 'disabled' }
-  }
+  default_scope { order(premium: :desc) }
+  scope :enabled,  lambda { select { |resource| resource.decorate.status == 'enabled' } }
+  scope :disabled, lambda { select { |resource| resource.decorate.status == 'disabled' } }
 
-  def status
-    if ends_at && DateTime.now.between?(starts_at, ends_at)
-      'enabled'
-    elsif DateTime.now.after?(starts_at) && ends_at.nil?
-      'enabled'
-    else
-      'disabled'
-    end
+  def disable!
+    self.ends_at = DateTime.now
+    save!
   end
 
-  def enabled?
-    true if status == 'enabled'
+  def enable!
+    self.starts_at = DateTime.now
+    self.ends_at = nil
+    save!
   end
 
   private
@@ -33,5 +28,6 @@ class Offer < ApplicationRecord
   def starts_at_before_ends_at
     errors.add(:ends_at, 'must be after starts_at') if ends_at && starts_at&.after?(ends_at)
   end
+
 
 end
