c58db76d7185cf8d72cc1f314143f70b46cc290e
Hugo Silva <hugocardososilva@gmail.com>
2020-09-29 01:51:08 -0300

    build(cl): default controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_path, alert: exception.message
+  end
 end
